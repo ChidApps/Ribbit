96ff540b5a6fb3a4a145a710d10ab443f23a54b8
ChidApps <chidapps.dev@gmail.com>
2014-10-18 23:28:45 -0500

    Made separate classes for each of the Friends and Inbox tabs.

---
+++ app/src/main/java/com/chidapps/android/ribbit/InboxFragment.java
@@ -0,0 +1,19 @@
+package com.chidapps.android.ribbit;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Carlos on 18/10/2014.
+ */
+public class InboxFragment extends ListFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_inbox, container, false);
+        return rootView;
+    }
+}
